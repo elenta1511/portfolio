a5eb95adec68296d73df01a0571bf23817a5d942
Elena Kuleshova <elenta1511@gmail.com>
2019-07-10 12:50:40 +0500

    паралакс верхний, адаптив и скилы, слайдер в отзывах частично

---
+++ src/scripts/reviews.js
@@ -0,0 +1,48 @@
+import Vue from 'vue';
+import Flickity from 'vue-flickity';
+
+const controls = {
+  template: '#reviews-controls'
+}
+
+new Vue({
+  el: "#reviews-component",
+  template: "#reviews-container",
+  components: {
+    Flickity,
+    controls
+  },
+  data() {
+    return {
+      flickityOptions: {
+        prevNextButtons: false,
+        pageDots: false,
+        wrapAround: true,
+        groupCells: window.screen.width > 700 ? 2 : 1,
+        resize: true
+      },
+      reviews: [] 
+    }
+  },
+  methods: {
+    next() {
+      this.$refs.flickity.next();
+    },
+    
+    previous() {
+      this.$refs.flickity.previous();
+    },
+    imagesRequired(data){
+      return data.map(item=>{
+        const requredImg = require(`../images/content/${item.photo}`);
+        item.photo = requredImg;
+
+        return item;
+      })
+    },
+  },
+  created(){
+    const data = require("../data/reviews.json");
+    this.reviews = this.imagesRequired(data);
+  }      
+});
